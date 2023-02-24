b05c6816da308956c440c1a8722dcaa74cb5d2d4
Roming22 <roming22@gmail.com>
2023-02-24 10:08:39 -0500

    Simplify key setup

--- seaks/buffer.py
+++ seaks/buffer.py
@@ -1,37 +1,43 @@
-from seaks.hardware.keys import oneshot, press, release
 
 
 class Buffer:
     _instance: "Buffer" = None
 
+    @classmethod
+    def instanciate(cls):
+        Buffer()
+
     def __init__(self):
+        if Buffer._instance is not None:
+            raise(RuntimeError, "Buffer object cannot be instanciated more than once.")
+        Buffer._instance = self
         self.keystrokes = []
         self.updated = False
 
         self.keys = {
             # Simple key
-            "A": self.clear_after(press("A")),
-            "a": self.clear_after(release("A")),
-            # "B": self.clear_after(press("B")),
-            # "b": self.clear_after(release("B")),
             # Chords
             "_+!": self.clear_after(lambda: True),
-            # Combo
-            "B+b": self.clear_after(oneshot("B")),
-            "@+B+C": oneshot("G"),
-            "C+c": self.clear_after(oneshot("C")),
-            "@+C+B": oneshot("G"),
-            # Sequence
-            "@+C+A": self.clear_after(lambda: True),
-            # Multi
-            "D+d": self.clear_after(oneshot("D")),
-            "E+e": self.clear_after(oneshot("E")),
-            "F+f": self.clear_after(oneshot("F")),
-            "@+F+E+D": oneshot("Z"),
-            # TapHold
-            # "B+!": press("X"),
-            # "@+F+E+!": oneshot("Y"),
+            # # Combo
+            # "B+b": self.clear_after(oneshot("B")),
+            # "@+B+C": oneshot("G"),
+            # "C+c": self.clear_after(oneshot("C")),
+            # "@+C+B": oneshot("G"),
+            # # Sequence
+            # "@+C+A": self.clear_after(lambda: True),
+            # # Multi
+            # "D+d": self.clear_after(oneshot("D")),
+            # "E+e": self.clear_after(oneshot("E")),
+            # "F+f": self.clear_after(oneshot("F")),
+            # "@+F+E+D": oneshot("Z"),
+            # # TapHold
+            # # "B+!": press("X"),
+            # # "@+F+E+!": oneshot("Y"),
         }
+
+    @classmethod
+    def register_event_sequence(cls, sequence, action):
+        cls._instance.keys[sequence] = action
 
     @classmethod
     def add(cls, key_name, value):
@@ -60,10 +66,12 @@ class Buffer:
                     return
         keystrokes.append(key_name)
 
-    def clear_after(self, action):
+    @classmethod
+    def clear_after(cls, action):
+        keystrokes = cls._instance.keystrokes
         def func():
             action()
-            self.keystrokes.clear()
+            keystrokes.clear()
 
         return func
 
@@ -87,4 +95,4 @@ class Buffer:
             cls._instance.updated = False
 
 
-Buffer._instance = Buffer()
+Buffer.instanciate()
--- seaks/features/key.py
+++ seaks/features/key.py
@@ -1,9 +1,15 @@
 from seaks.action import Action
 from seaks.event import Trigger
 from seaks.state import State
+from seaks.buffer import Buffer
+from seaks.hardware.keys import oneshot, press, release
+
 
 
 class Key:
     def __init__(self, switch: str, key_name: str, layer: State) -> None:
+        key_name = str.upper(key_name)
         layer.add_trigger(Trigger(f"switch.{switch}", True), Action.press(key_name))
         layer.add_trigger(Trigger(f"switch.{switch}", False), Action.release(key_name))
+        for event_sequence, action in [(key_name, Buffer.clear_after(press(key_name))), (str.lower(key_name), Buffer.clear_after(release(key_name)))]:
+            Buffer.register_event_sequence(event_sequence, action)
