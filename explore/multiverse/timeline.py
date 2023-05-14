c976fa18f0c96a77fac59544e071d4d04dfa3c7f
Roming22 <roming22@gmail.com>
2023-05-14 09:26:11 -0400

    Working with unittests

--- explore/multiverse/timeline.py
+++ explore/multiverse/timeline.py
@@ -3,6 +3,20 @@ class Timeline:
     next_timeline = None
 
     def __init__(self, events, output=[], status=False) -> None:
+        """Create a new timeline
+
+        events: an ordered list of expected events in the timeline. Each event
+                is paired with the action that should be executed when the event
+                is triggered.
+                The timeline will be terminated if it receives an event that is
+                not part of the timeline.
+        output: an order list of actions that will be executed if the timeline
+                is selected when all the timeline are resolved time.
+        status: a boolean flag that indicates whether the timeline has come to
+                some sort of resolution. This usually means that a character has
+                been output or there was a layer change.
+                A resolved timeline will tend to spawn new timelines.
+        """
         self.events = events
         if output:
             self.output = list(output)
@@ -12,26 +26,42 @@ class Timeline:
         self.active_timelines.append(self)
 
     def split(self) -> list:
+        """Create a new timeline"""
         new_timeline = Timeline(self.events, self.output, self.status)
         return [self, new_timeline]
 
     def _process_event(self, event) -> None:
-        if event != self.events.pop(0):
-            self.active_timeline.remove(self)
+        """Process an event
+
+        The event is checked against the timeline expectation.
+        If the event is not part of the timeline, the timeline is terminated.
+        If the event is part of the timeline, the associated action is executed.
+        """
+        print("GRNX", self.events)
+        expected_event, action = self.events.pop(0)
+        if event != expected_event:
+            self.active_timelines.remove(self)
+        if action:
+            action()
 
     @classmethod
     def process_event(cls, event):
+        """Process an event in all active timelines
+
+        That event might trigger a timeline resolution, in which case
+        the timeline resolving the sequence of events is executed.
+        """
         for timeline in list(cls.active_timelines):
             timeline._process_event(event)
         if False not in [t.status for t in cls.active_timelines]:
             cls.collapse()
 
     @classmethod
-    def collapse(cls):
-        print("Collapsing timelines")
+    def resolve(cls):
+        """Execute the action of the timeline and collapse the multiverse to a single point"""
+        print("Resolving timelines")
         timeline = cls.next_timeline
         for action in timeline.output:
             action()
         timeline.output.clear()
         cls.active_timeline = [timeline]
-    
--- explore/tests/test_timeline.py
+++ explore/tests/test_timeline.py
@@ -1,31 +1,47 @@
-import unittest
+from unittest.mock import MagicMock, call
 
-from multiverse.timeline import Timeline
+from explore.multiverse.timeline import Timeline
 
 
-class TestTimeline(unittest.TestCase):
-    def test_new(self):
-        """
-        Test the creation of a Timeline
-        """
-        events = [1, 2, 3]
-        timeline = Timeline(events)
-        self.assertEqual(timeline.events, [1, 2, 3])
-        self.assertEqual(timeline.output, [])
-        self.assertEqual(timeline.status, False)
+def test_new():
+    """
+    Test the creation of a Timeline
+    """
+    timeline_events = [(1, None)]
+    timeline = Timeline(timeline_events)
+    assert timeline.events == timeline_events
+    assert timeline.output == []
+    assert timeline.status == False
 
-    def test_split(self):
-        """
-        Test a timeline split
-        """
-        events = [1, 2, 3]
-        timeline = Timeline(events)
-        timeline.output=["A", "B"]
-        timeline.status = True
-        split_time = timeline.split()[-1]
-        self.assertEqual(split_time.events, [1, 2, 3])
-        self.assertEqual(split_time.output, timeline.output)
-        self.assertEqual(split_time.status, timeline.status)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_split():
+    """
+    Test a timeline split
+    """
+    events = [(1, None), (2, None)]
+    timeline = Timeline(events)
+    timeline.output = ["A", "B"]
+    timeline.status = True
+    split_time = timeline.split()[-1]
+    assert split_time.events == events
+    assert split_time.output == timeline.output
+    assert split_time.status == timeline.status
+
+
+def test_simplekey_timeline():
+    action = MagicMock()
+    timeline_events = [(1, lambda: action("A")), (2, lambda: action("a"))]
+    events = [1, 2]
+    run_scenario(timeline_events, events)
+    assert action.call_args_list == [call("A"), call("a")]
+
+
+def run_scenario(timeline_events, events):
+    # Reset Timeline
+    Timeline.active_timelines.clear()
+    Timeline.next_timeline = None
+
+    # Run test
+    Timeline(timeline_events)
+    for event in events:
+        Timeline.process_event(event)
